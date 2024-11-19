98a8cf4f11743b0413ed9b5471dd0fe9ac961fd1
SophieCarpentier <sophiesakuracarpentier@gmail.com>
2024-11-19 15:29:23 +0100

    test

---
+++ script/test.js
@@ -0,0 +1,12 @@
+
+//console.log("Hello World!");
+
+//let userName = "NomUtilisateur"; //string
+//let age = 19; //int
+let size = 1.55; //float
+//let isCool = true; //bool
+
+let a = 15
+let b = 35
+
+console.log(a+b, a/b, a-b, a*b, a%b);
