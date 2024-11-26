af8fb0ea0b55aaa3cc07e89a425da4ef47ef1881
SophieCarpentier <sophiesakuracarpentier@gmail.com>
2024-11-26 16:38:47 +0100

    avancement

--- script/test.js
+++ script/test.js
@@ -1,12 +1,30 @@
+let number1 = document.querySelector('#Number1');
+let number2 = document.querySelector('#Number2');
+let calculate = document.querySelector('#calculate');
+let resultHtml = document.querySelector('#result');
 
-//console.log("Hello World!");
 
-//let userName = "NomUtilisateur"; //string
-//let age = 19; //int
-let size = 1.55; //float
-//let isCool = true; //bool
 
-let a = 15
-let b = 35
+function makeCalcul() {
+    let nombre1 = parseInt(Number1.value);
+    
+    let nombre2 = parseInt(Number2.value);
+    
+    let resultat = nombre1 + nombre2;
+    let messageAddition = `<p>Le résultat de l'addition de ${nombre1} et de ${nombre2} est ${resultat}</p>`;
+    resultHtml.innerHTML = messageAddition;
+            
+    resultat = nombre1 - nombre2;
+    let messageSoustraction = `<p>Le résultat de la soustraction de ${nombre1} et de ${nombre2} est ${resultat}</p>`;
+    resultHtml.innerHTML += messageSoustraction;
+            
+    resultat = nombre1 * nombre2;
+    let messageMultiplication = `<p>Le résultat de la multiplication de ${nombre1} et de ${nombre2} est ${resultat}</p>`;
+    resultHtml.innerHTML += messageMultiplication;
+            
+    resultat = nombre1 / nombre2;
+    let messageDivision = `<p>Le résultat de la division de ${nombre1} et de ${nombre2} est ${resultat}</p>`;
+    resultHtml.innerHTML += messageDivision;
+}
 
-console.log(a+b, a/b, a-b, a*b, a%b);
+calculate.addEventListener("click", makeCalcul)
